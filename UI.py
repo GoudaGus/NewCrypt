1ba1d91c616afa8a654d23c37d731c21a59ed7be
Brandon Parks <brandon.parks.0708@mail.linnbenton.edu>
2020-05-19 17:38:51 -0700

    Get Beta working

--- UI.py
+++ UI.py
@@ -3,7 +3,6 @@ from tkinter import filedialog
 from os import environ
 from encrypt import encrypt, decrypt
 from digest import encode, decode
-# Label, Text, Tk, Button, Entry
 
 
 class Initial_IO:
@@ -11,15 +10,17 @@ class Initial_IO:
     def __init__(self, master):
 
         def file_explorer():
-            filename = filedialog.askopenfilename(initialdir = f"{environ['USERPROFILE']}/Documents",
-                                            title = "Select File",
-                                            filetypes = (("Text files", 
-                                                        "*.txt*"), 
-                                                       ("all files", 
-                                                        "*.*")))                             
+            filename = filedialog.askopenfilename(
+                    initialdir=f"{environ['USERPROFILE']}/Documents",
+                    title="Select File",
+                    filetypes=(("Text files",
+                                "*.txt*"),
+                                ("all files",
+                                "*.*")))
             return filename
             
         def get_file(self):
+            self.file_box.delete(0, END)
             self.file_box.insert(0, str(file_explorer()))
 
 
@@ -34,29 +35,55 @@ class Initial_IO:
         self.file_button.grid(column=1, row=1, sticky = W)
 
         self.file_box = Entry(master)
-        self.file_box.grid(column = 1, padx = 85, row = 1, sticky = W)
+        self.file_box.grid(column = 1, ipadx = 70, padx = 85, row = 1, sticky = W)
 
         self.encrypt_button = Button(master,
                                     text = "Encrypt",
-                                    command = self.init_encrypt).grid(column = 1, row = 2, pady = 10, padx = 90, sticky = W)
+                                    command = self.init_encrypt
+                                    )
+        self.encrypt_button.grid(
+                                column = 1,
+                                row = 2,
+                                pady = 10,
+                                padx = 90,
+                                sticky = W
+                                )
 
         self.decrypt_button = Button(master,
                                     text = "Decrypt",
-                                    command = self.init_decrypt).grid(column = 1, row = 2, pady = 10, padx = 150, sticky = W)
+                                    command = self.init_decrypt
+                                    )
+        self.decrypt_button.grid(
+                                column = 1,
+                                row = 2,
+                                pady = 10,
+                                padx = 150,
+                                sticky = W
+                                )
 
         self.error_label = Label(master, text = "")
-        self.error_label.grid(column = 3, row = 1)
+        self.error_label.grid(
+                            column = 1,
+                            # columnspan = 2,
+                            padx = 250,
+                            row = 2,
+                            sticky = W
+                            )
 
         self.output_label = Label(master, text = "Output:")
         self.output_label.grid(column = 1, row = 3, sticky = W)
         self.output_text = Text(master, height = 7, width = 50)
-        self.output_text.grid(column = 1, columnspan = 2, pady = 10, row = 3, sticky = W)
+        self.output_text.grid(column = 1, columnspan = 2, pady = 10, padx = 20, row = 3, sticky = W)
         self.output_text_scroll = Scrollbar(master, command = self.output_text.yview)
-        self.output_text_scroll.grid(column = 3, row = 3, sticky = W)
+        self.output_text_scroll.grid(column = 1, row = 3, sticky = W)
         self.output_text.configure(yscrollcommand = self.output_text_scroll.set)
 
     def write_error(self, error=""):
         self.error_label.configure(text = error)
+
+    def write_output(self, info=""):
+        self.output_text.insert(END, str(info))
+        
 
     def open_file(self, filename, action, contents=None):
         if action == "w":
@@ -64,16 +91,20 @@ class Initial_IO:
                 with open(filename, action) as openfile:
                     openfile.write(contents)
                     openfile.close()
-            except FileNotFoundError:
+            except OSError:
+                self.write_error("Error: Invalid file location")
                 return False
         else:
             try:
                 with open(filename, action) as openfile:
                     temp = openfile.read()
                     openfile.close()
-                    print(temp)
                     return temp
             except FileNotFoundError:
+                self.write_error("Error: File not found")
+                return False
+            except OSError:
+                self.write_error("Error: Invalid file location")
                 return False
 
     def get_save_path(self):
@@ -86,72 +117,46 @@ class Initial_IO:
         # try:
         self.save_file_path = self.file_path[0:len(self.file_path) - o]
         self.file_name = self.file_path[len(self.file_path) - o:len(self.file_path) - 4]
-        print(self.save_file_path)
-        print(self.file_name)
         # except:
         #     exit()
     
     def init_encrypt(self):
-        
+        self.get_save_path()
         orig_file = self.open_file(self.file_path, "r")
         if orig_file:
-            self.get_save_path()
             self.write_error()
             m = encode(str(orig_file))
             output = encrypt(m)
             self.encrypted = output
-            self.output_text.insert(END, f"{self.encrypted}")
+            self.write_output(self.encrypted)
             self.save_file_path = str(self.save_file_path + self.file_name + "_encrypted.txt")
             self.open_file(self.save_file_path, "w", self.encrypted)
-            
-        else:
-            self.write_error("Error: File not found.")
 
     def init_decrypt(self):
         self.get_save_path()
         self.write_error()
-        self.file_path
         ciph_file = self.open_file(self.file_box.get(), "r")
-        temp0 = 0
-        temp1 = []
-        for i in ciph_file:
-            temp0 += 1
-            if i == ",":
-                temp1.append(temp0)
-        c = int(ciph_file[0:temp1[0] - 1])
-        d = int(ciph_file[temp1[0] + 1:temp1[1] - 1])
-        n = int(ciph_file[temp1[1] + 1:len(ciph_file)])
-        ck = (c, (d, n))
-        print(ck)
-        encoded_text = decrypt(ck)
-        plain_text = decode(encoded_text)
-        print(plain_text)
+        try:
+            if ciph_file:
+                temp0 = 0
+                temp1 = []
+                for i in ciph_file:
+                    temp0 += 1
+                    if i == ",":
+                        temp1.append(temp0)
+                c = int(ciph_file[0:temp1[0] - 1])
+                d = int(ciph_file[temp1[0]:temp1[1] - 1])
+                n = int(ciph_file[temp1[1]:len(ciph_file)])
+                ck = (c, (d, n))
 
-        print(c)
-        # key1 = False
-        # key2 = False
-        # temp0 = ""
-        # temp1 = ""
-        # temp2 = ""
 
-        # for i in self.encrypted:
-        #     if i == "," and not key1:
-        #         key1 = True
-        #     elif i == "," and key1:
-        #         key1 == False
-        #         key2 = True
-        #     if not key1 and not key2 and not i == ",":
-        #         temp_m += i
-        #     if key1 and not i == ",":
-        #         temp_d += i
-        #     if key2 and not i == ",":
-        #         temp_n += i
-        # for i in 
-        # self
-
-        # print 
-        # temp1 = (temp0, temp1, temp2)
-        print(temp1)
+            encoded_text = decrypt(ck)
+            plain_text = decode(encoded_text)
+            decrypted_save_path = self.save_file_path + self.file_name + "_decrypted.txt"
+            self.write_output(plain_text)
+            self.open_file(decrypted_save_path, "w", plain_text)
+        except IndexError:
+            self.write_error("Error: Invalid encrypted file")
 
 
 
